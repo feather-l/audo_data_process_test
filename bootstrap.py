363630131d9400db4dee0aedcb56c2538efd3864
feather-l <lingfeather@126.com>
2026-04-07 17:48:52 +0800

    update

---
+++ bootstrap.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File	:	bootstrap.py
+@Time	:	2026/04/07 15:02:54
+@Autho	:	zhangtonghui
+@Desc	:	实现整体数据处理流程的控制
+'''
+
+class ModuleGateway:
+    def __init__(self):
+        pass
+    
+    async def bootstrap(self):
+        pass
---
+++ module_manager.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File	:	module_manager.py
+@Time	:	2026/04/07 15:04:56
+@Autho	:	zhangtonghui
+@Desc	:	实现模块管理器，负责协调各个模块的运行和数据流转
+'''
+
+class ModuleManager:
+    def __init__(self):
+        pass
+    
+    async def run(self):
+        pass
