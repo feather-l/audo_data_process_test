15af8a77f18b6ee8faaf2ec0de1adcc33716e623
feather-l <lingfeather@126.com>
2026-04-07 19:02:39 +0800

    update

---
+++ events.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File	:	events.py
+@Time	:	2026/04/07 18:12:34
+@Autho	:	zhangtonghui
+@Desc	:	定义事件系统，支持模块之间的事件通信和数据传递，参考https://github.com/openxjarvis/openclaw-python/blob/3e779b92/openclaw/events.py#L47-L154
+'''
+
